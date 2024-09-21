444458df0f4f30fd221f255c1b8c6ea6fa3c0f75
LuisLÃozano0144 <loznao44@gmail.com>
2024-09-21 14:15:13 -0500

    primer commit

---
+++ app/src/main/java/components/text.kt
@@ -0,0 +1,32 @@
+package components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview(showBackground = true)
+fun GreetingWithSurface() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "¡Bienvenido a la app!")
+            Text(text = "Este es un ejemplo de Surface.")
+            Button(onClick = { /* Acción */ }) {
+                Text("Acción")
+            }
+        }
+    }
+}
+
